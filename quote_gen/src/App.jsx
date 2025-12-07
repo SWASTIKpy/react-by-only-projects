688103f0e8d47a1596ae82ead882ce167e80f024
swastikPY <swastikvishwa908@gmail.com>
2025-12-07 10:12:50 +0530

    added quote gen

---
+++ quote_gen/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-amber-400'>yayyy</h1>
+    </>
+  )
+}
+
+export default App
